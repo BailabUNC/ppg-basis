33395b7524565625f3000b971a103f3240c73096
ArjunPutcha <arjunputcha@gmail.com>
2025-07-18 14:34:50 -0400

    plot cost space

--- ppg_basis/ppg_extractor.py
+++ ppg_basis/ppg_extractor.py
@@ -3,6 +3,8 @@ from scipy.optimize import differential_evolution, minimize
 from ppg_basis.utils.generator_utils import *
 from ppg_basis.model import *
 from ppg_basis.cost import objective_function
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class ppgExtractor:
     def __init__(self, signal: np.ndarray, fs: float, hr: float, sigma: float, L: int,
@@ -141,3 +143,76 @@ class ppgExtractor:
             theta_phase2 = res.x[:self.L]
             params_phase2 = res.x[self.L:].reshape((self.L, P))
         return theta_phase2, params_phase2
+
+    def plot_cost_landscape(self, mse_flag: bool = True, corr_flag: bool = False, appg_flag: bool = False, resolution: int = 10):
+        P = self.params.shape[1]
+        theta_bounds = self.bounds[:self.L]
+        param_bounds = self.bounds[self.L:]
+
+        for i in range(self.L):
+            print(f"Basis {i + 1}/{self.L}")
+
+            # Parameter ranges
+            theta_range = np.linspace(theta_bounds[i][0], theta_bounds[i][1], resolution)
+            p_ranges = [
+                np.linspace(param_bounds[i * P + j][0], param_bounds[i * P + j][1], resolution)
+                for j in range(P)
+            ]
+            fixed_vals = [np.mean(param_bounds[i * P + j]) for j in range(P)]
+
+            X, Y, Z, C, S = [], [], [], [], []
+
+            for theta in theta_range:
+                for p1 in p_ranges[0]:
+                    for p2 in p_ranges[1]:
+                        for p3 in (p_ranges[2] if P > 2 else [None]):
+                            for p4 in (p_ranges[3] if P > 3 else [None]):
+                                thetas = self.thetai.copy()
+                                thetas[i] = theta
+                                params = self.params.copy()
+
+                                param_vec = [p1, p2]
+                                if P > 2:
+                                    param_vec.append(p3 if p3 is not None else fixed_vals[2])
+                                if P > 3:
+                                    param_vec.append(p4 if p4 is not None else fixed_vals[3])
+                                params[i] = param_vec
+
+                                x_full = np.concatenate([thetas, params.flatten()])
+                                model_ppg = unified_model_ode(
+                                    ppinterval=self.pp_interval,
+                                    fs=self.fs,
+                                    seconds=self.rr_interval,
+                                    basis_type=self.basis_type,
+                                    thetai=thetas,
+                                    basis_params=params
+                                )
+
+                                cost_total = objective_function(model_ppg, self.signal,
+                                                                mse_flag=mse_flag,
+                                                                corr_flag=corr_flag,
+                                                                appg_flag=appg_flag)
+
+                                X.append(theta)
+                                Y.append(p1)
+                                Z.append(p2)
+                                # Size reflects 4th parameter if present
+                                if P > 3:
+                                    size_val = p4
+                                elif P > 2:
+                                    size_val = p3
+                                else:
+                                    size_val = 0.5  # default size
+                                C.append(cost_total)
+                                S.append(40 + 80 * size_val)
+
+            fig = plt.figure(figsize=(10, 7))
+            ax = fig.add_subplot(111, projection='3d')
+            sc = ax.scatter(X, Y, Z, c=C, s=S, cmap='plasma', alpha=0.8)
+            ax.set_title(f"Full Cost Landscape for Basis {i + 1}")
+            ax.set_xlabel("Theta")
+            ax.set_ylabel("Param 1")
+            ax.set_zlabel("Param 2")
+            fig.colorbar(sc, ax=ax, shrink=0.6, label="Total Cost")
+            plt.tight_layout()
+            plt.show()
