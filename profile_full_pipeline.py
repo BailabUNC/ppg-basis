858ba3a75b2073255b9fde32aa51a9c4938098ae
Rachit Keyal <rachitkeyal@gmail.com>
2025-07-20 16:38:51 -0500

    clean up snakeviz scripts

---
+++ profile_full_pipeline.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+import webbrowser
+import time
+import fnmatch
+import glob
+
+# find nbs
+notebooks = [f for f in os.listdir(".")
+             if fnmatch.fnmatch(f, "testbench_*.ipynb")]
+
+if not notebooks:
+    print("No matching notebooks found.")
+    exit(1)
+
+# run nbs
+for nb in notebooks:
+    print(f"Executing notebook: {nb}")
+    subprocess.run([
+        "jupyter", "nbconvert", "--to", "notebook", "--execute",
+        "--inplace", os.path.join(".", nb)
+    ], check=True)
+
+# find profs and open
+prof_files = glob.glob(os.path.join("profile_output", "**", "*.prof"), recursive=True)
+for i, prof in enumerate(prof_files):
+    port = 8000 + i
+    print(f"Launching {prof} on http://localhost:{port}/snakeviz/")
+    
+    subprocess.Popen(["snakeviz", "-p", str(port), prof])
+    time.sleep(1)
+    webbrowser.open(f"http://localhost:{port}/snakeviz/")
---
+++ view_profiles.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import webbrowser
+import time
+import glob
+
+# find profs and open
+prof_files = glob.glob(os.path.join("profile_output", "**", "*.prof"), recursive=True)
+for i, prof in enumerate(prof_files):
+    port = 8000 + i
+    print(f"Launching {prof} on http://localhost:{port}/snakeviz/")
+    
+    subprocess.Popen(["snakeviz", "-p", str(port), prof])
+    time.sleep(1)
+    webbrowser.open(f"http://localhost:{port}/snakeviz/")
