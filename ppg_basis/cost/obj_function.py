28dc54a7c1a2d94c734dba2e4637ce17275a9212
Rachit Keyal <rachitkeyal@gmail.com>
2025-07-29 19:58:51 -0400

    Fix logic in obj_function.py

--- ppg_basis/cost/obj_function.py
+++ ppg_basis/cost/obj_function.py
@@ -3,8 +3,7 @@ from numba import njit
 
 @njit
 def objective_function(model, signal, cost_metrics: list, func = None):
-    cost_metrics = terms.copy()
     obj_func = func(model, signal) if func is not None else 0
     for metric in cost_metrics:
-        obj_func += cost_metrics[metric(model, signal)]
+        obj_func += terms[metric](model, signal)
     return obj_func
