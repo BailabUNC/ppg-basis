9ef7f98633809e5d430cb1d86ea18d959044f219
Rachit Keyal <rachitkeyal@gmail.com>
2025-07-30 11:52:53 -0400

    Documentation for objective function

--- ppg_basis/cost/obj_function.py
+++ ppg_basis/cost/obj_function.py
@@ -3,6 +3,14 @@ from numba import njit
 
 @njit
 def objective_function(model, signal, cost_metrics: list, func = None):
+    """
+    Returns objective function by combining cost metrics
+    :param model: reference signal
+    :param signal: reconstructed signal
+    :param cost_metrics: list of metrics to ccombine
+    :param func: optional cost function
+    :return: objective function
+    """
     obj_func = func(model, signal) if func is not None else 0
     for metric in cost_metrics:
         obj_func += terms[metric](model, signal)
