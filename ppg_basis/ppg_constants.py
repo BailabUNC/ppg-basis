df2aba2c1599c3047587d9431771f18546962a4e
ArjunPutcha <arjunputcha@gmail.com>
2026-03-21 12:27:48 -0400

    constants file

---
+++ ppg_basis/ppg_constants.py
@@ -0,0 +1,54 @@
+# Constant values used throughout codebase
+default_M = 512
+
+# used to generate bounds and constraints for optimization
+param_bnds_dict = {
+    'gaussian' : [(0.0, 1.0), (0.05, 3.0)],
+    'gamma' : [(0.0, 1.0), (1.0, 6.0), (0.05, 3.0)],
+    'skewed-gaussian' : [(0.0, 1.0), (0.05, 3.0), (-10.0, 10.0)]
+}
+
+basis_types = ['gaussian', 'gamma', 'skewed-gaussian']
+
+default_solver = "basis"
+
+# default parameters
+default_params = {
+    # shared parameters
+    "fs" : 60,
+    "basis_type" : "gaussian",
+    "L" : 2,
+    "mu" : 1,
+    "sigma" : 0,
+    "solver" : default_solver,
+
+    # extractor-specific parameters
+    "cost_metrics" : ["mse", "corr"],
+    "cost_weights" : [1.0, 1.0],
+    "cost_func" : None,
+
+    # generator-specific parameters
+    "hr" : 60,
+    "duration" : 1,
+    "M" : default_M,
+}
+
+param_validators = {
+    # shared parameters
+    "fs" : lambda x: isinstance(x, int) and x > 0,
+    "basis_type" : lambda x: x in basis_types,
+    "L" : lambda x : isinstance(x, int) and x > 1,
+    "mu" : lambda x: x > 0,
+    "sigma" : lambda x : x >= 0,
+    "solver" : lambda x : isinstance(x, str),
+
+    # extractor-specific parameters
+    "cost_metrics" : lambda x : isinstance(x, list) and all(isinstance(metric, str) for metric in x),
+    "cost_weights": lambda x : isinstance(x, list) and all(isinstance(weight, (int, float)) for weight in x),
+    "cost_func" : lambda x : callable(x),
+
+    # generator-specific parameters
+    "hr" : lambda x : isinstance(x, int) and x > 0,
+    "duration" : lambda x : x > 0,
+    "M" : lambda x : isinstance(x, int) and x > 0,
+}
